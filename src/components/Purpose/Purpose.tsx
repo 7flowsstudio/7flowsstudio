87c946a6ca4cc39d0a9e8d14e579bf7b1dd9de27
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2025-11-12 18:47:30 +0200

    css-text-purpose

--- src/components/Purpose/Purpose.tsx
+++ src/components/Purpose/Purpose.tsx
@@ -6,20 +6,23 @@ const Purpose = () => {
   const t = useTranslations("Purpose");
   return (
     <div className={s.section}>
-      <div>
-        <p>
+      <div className={s.container}>
+        <p className={s.text_1}>
           {t("text_1")}
           <svg className={s.iconEmpty}>
             <use href="/img/purpose/empty.svg"></use>
           </svg>
         </p>
-        <p>
+        <p className={s.text_2}>
           <svg className={s.iconArrow}>
             <use href="/img/purpose/arrow.svg"></use>
           </svg>
           {t("text_2")}
         </p>
+        <div className={s.wrappText}>
         <p>{t("text_4")}</p>
+        <p className={s.text_3}>{t("text_3")}</p>
+        </div>
         <button className={s.primaryBtn}>
           <span>{t("button")}</span>
           <div className={s.primaryWrapIcon}>
@@ -28,7 +31,6 @@ const Purpose = () => {
             </svg>
           </div>
         </button>
-        <p>{t("text_3")}</p>
       </div>
     </div>
   );
