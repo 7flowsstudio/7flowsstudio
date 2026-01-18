c3d7e4d78305adf4d157ba55d7801a572740db60
Viktoriia <struk.victoria@gmail.com>
2026-01-18 23:41:11 +0200

    add review component

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,6 +3,7 @@ import Flows from "@/components/Sections/Flows/Flows";
 import Hero from "@/components/Sections/Hero/Hero";
 import Portfolio from "@/components/Sections/Portfolio/Portfolio";
 import Services from "@/components/Services/Services";
+import ReviewsSlider from "@/components/Sections/Reviews/ReviewsSlider";
 import Stages from "@/components/Stages/Stages";
 import { Locale } from "@/i18n/routing";
 import { setRequestLocale } from "next-intl/server";
@@ -24,6 +25,7 @@ export default async function IndexPage({ params }: Props) {
 			<Flows />
 			<Stages />
 			<Services />
+			<ReviewsSlider/>
 			<Purpose />
 		</>
 	);
