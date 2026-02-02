0a5d3d39a23c4891d5e06d26b4956abe2573a1b1
7flowsstudio <seven.flows.studio@gmail.com>
2026-02-02 18:52:12 +0100

    add corect footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,27 +2,16 @@
 import WrapperForComponents from "@/lib/utils/WrapperForComponents/WrapperForComponents";
 import React from "react";
 import s from "./Footer.module.css";
-import { useTranslations } from "next-intl";
 import useIsMobile from "@/lib/utils/isMobile/isMobile";
 import DesktopFooter from "./DesktopFooter/DesktopFooter";
 import MobileFooter from "./MobileFooter/MobileFooter";
 
 const Footer = () => {
-	const t = useTranslations("Footer");
 	const isMobile = useIsMobile();
-
-	// if (!isMobile) return 0;
 
 	return (
 		<div className={s.foneWrapper}>
-			<div className={`${s.foneElipse} ${s.adWrap}`}>
-				{/* <Image
-					src="/img/footer/Ellipse.svg"
-					fill
-					className={s.image}
-					alt="img_fone"
-				/> */}
-			</div>
+			<div className={`${s.foneElipse} ${s.adWrap}`}></div>
 			<WrapperForComponents paddingBottom={isMobile ? 32 : 64}>
 				<div className={s.footerWrapper}>
 					<MobileFooter />
