a7771e3927047db07b98be30cf966af0cf5a63c8
Viktoriia <struk.victoria@gmail.com>
2026-02-05 18:25:12 +0200

    update styles for services section

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -1,7 +1,5 @@
-import WrapperForComponents from "@/lib/utils/WrapperForComponents/WrapperForComponents";
-import React from "react";
-import s from "./Services.module.css";
 import { useTranslations } from "next-intl";
+import s from "./Services.module.css";
 
 const Services = () => {
 	const t = useTranslations("Services");
@@ -59,77 +57,71 @@ const Services = () => {
 	};
 
 	return (
-		// <WrapperForComponents>
-			<div id="services" className={s.servicesWrapper}>
-				<h2 className={s.title}>{t("title")}</h2>
-				<ul className={s.servicesList}>
-					{servicesList.map((item) => (
-						<li key={item.id} className={s.servicesItem}>
-							<div className={s.servicesBlock}>
-								<div className={s.titleBlock}>
-									<div className={s.typeBlock}>
-										{item.id === 1 && (
-											<svg className={s.iconTypeUnion}>
-												<use href="/sprite.svg#icon-union"></use>
-											</svg>
-										)}
+			<section id="services" className={s.servicesSection}>
+				<div className={s.servicesWrapper}>
+					<h2 className={s.title}>{t("title")}</h2>
+					<ul className={s.servicesList}>
+						{servicesList.map((item) => (
+							<li key={item.id} className={`${s.servicesItem} ${item.id === 1 ? s.servicesItemPopular : s.servicesItemSide}`}>
+								<div className={s.servicesBlock}>
+									{/* Popular badge for middle card */}
+									{item.id === 1 && (
+										<div className={s.popularBadge}>
+											<span>Найпопулярніша</span>
+										</div>
+									)}
 
-										<h3 className={s.servicesTitle}>{item.name}</h3>
+									{/* Service type */}
+									<div className={s.serviceType}>
+										{item.type}
 									</div>
-									<div className={s.servicesPrice}>
-										<h4 className={s.priceTitle}>{item.from}</h4>
-										<p className={s.typeTitle}>{item.type}</p>
+
+									{/* Price as main focus */}
+									<div className={s.priceBlock}>
+										<div className={s.priceText}>{item.from}</div>
+										<div className={s.priceAccent}></div>
+									</div>
+
+									{/* Features list */}
+									<div className={s.featuresBlock}>
+										<ul className={s.servicesInerList}>
+											{item.list.map((service, index) => (
+												<li key={index} className={`${s.servicesInerItem} ${isNotService(item.id, index) ? s.featureDisabled : s.featureIncluded}`}>
+													<div className={s.iconBlock}>
+														<svg className={s.iconCheck}>
+															<use
+																href={
+																	isNotService(item.id, index)
+																		? "/sprite.svg#icon-not-check-list"
+																		: "/sprite.svg#icon-check-list"
+																}
+															></use>
+														</svg>
+													</div>
+													<p className={s.inerText}>{service}</p>
+												</li>
+											))}
+										</ul>
 									</div>
 								</div>
-								<ul className={s.servicesInerList}>
-									{item.list.map((service, index) => (
-										<li key={index} className={s.servicesInerItem}>
-											<div className={s.iconBlock}>
-												<svg className={s.iconCheck}>
-													<use
-														href={
-															isNotService(item.id, index)
-																? "/sprite.svg#icon-not-check-list"
-																: "/sprite.svg#icon-check-list"
-														}
-													></use>
-												</svg>
-											</div>
-											<p className={s.inerText}>{service}</p>
-										</li>
-									))}
-								</ul>
-							</div>
-							{/* <button
-								type="button"
-								className={s.linkBtn}
-								onClick={() => {
-									const contactsSection = document.getElementById('contacts');
-									if (contactsSection) {
-										contactsSection.scrollIntoView({ behavior: 'smooth' });
-									}
-								}}
-							>
-								{t("btn")}
-							</button> */}
-						</li>
-					))}
-				</ul>
-				<div className={s.sale}>
-					<div className={s.iconWrapper}>
-						<svg className={s.iconUnion}>
-							<use href="/sprite.svg#icon-union"></use>
-						</svg>
-					</div>
-					<span className={s.saleUnion}>-50% {t("title_sale")}</span>
-					<div className={s.iconWrapper}>
-						<svg className={s.iconUnion}>
-							<use href="/sprite.svg#icon-union"></use>
-						</svg>
+							</li>
+						))}
+					</ul>
+					<div className={s.sale}>
+						<div className={s.iconWrapper}>
+							<svg className={s.iconUnion}>
+								<use href="/sprite.svg#icon-union"></use>
+							</svg>
+						</div>
+						<span className={s.saleUnion}>-50% {t("title_sale")}</span>
+						<div className={s.iconWrapper}>
+							<svg className={s.iconUnion}>
+								<use href="/sprite.svg#icon-union"></use>
+							</svg>
+						</div>
 					</div>
 				</div>
-			</div>
-		// </WrapperForComponents>
+			</section>
 	);
 };
 
