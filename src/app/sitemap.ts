6da45f48fa10dad4550c95a7078f076f018c9c39
Viktoriia <struk.victoria@gmail.com>
2026-02-20 22:07:14 +0200

    feat: add dynamic sitemap for existing pages with locales

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,20 @@ import {host} from '@/config';
 import {Locale, getPathname, routing} from '@/i18n/routing';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  return [getEntry('/')];
+
+  const publicPages: Array<Parameters<typeof getPathname>[0]['href']> = [
+    '/',
+    '/policy',
+    '/terms'
+  ];
+
+  const entries: MetadataRoute.Sitemap = [];
+
+  publicPages.forEach(href => {
+    entries.push(getEntry(href));
+  });
+
+  return entries;
 }
 
 type Href = Parameters<typeof getPathname>[0]['href'];
@@ -11,6 +24,9 @@ type Href = Parameters<typeof getPathname>[0]['href'];
 function getEntry(href: Href) {
   return {
     url: getUrl(href, routing.defaultLocale),
+    lastModified: new Date(),
+    changeFrequency: 'monthly' as const,
+    priority: href === '/' ? 1.0 : 0.2,
     alternates: {
       languages: Object.fromEntries(
         routing.locales.map((locale) => [locale, getUrl(href, locale)])
